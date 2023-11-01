7acd8e1492a4280df273426e8b6ccfa96b82322d
Abhinandan B M <72445333+AbhinandanBM@users.noreply.github.com>
2023-11-01 14:49:58 +0000

    datatypes added

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "Abhi@google.com"
+var accountPassword = "12345"
+accountCity = "Bidadi"
+let accountState ;
+
+// accountId = 2 // not allowed
+
+accountEmail = "abm@bm.com"
+accountPassword = "12221221"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3);//Code readability should be high
+
+console.log("Abhi");
+
+let name = "Abhinandan"
+let age = 19
+let loggedIn = true
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not assingned any value
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
