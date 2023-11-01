64b1b3737fbc81ae34a187b82fcf41b91abb3dc5
Abhinandan B M <72445333+AbhinandanBM@users.noreply.github.com>
2023-11-01 16:02:00 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueinNumber = Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+//"33" => 33
+//"33ab" => NaN
+//true => 1; false => 0
+//null => 0
+//undefined => NaN
+
+let loggedIn = "Abhi"
+
+let booleanloggedIn = Boolean(loggedIn)
+// console.log(booleanloggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"Abhi" => true
+
+let someNumber = 12
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
