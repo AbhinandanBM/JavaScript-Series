1cc46907846cc3c462b7d12336098960dc0c3884
AbhinandanBM <bmabhi13@gmail.com>
2023-12-07 21:27:53 +0530

    iteration

---
+++ 05_Iterations/02_whileloop.js
@@ -0,0 +1,25 @@
+// While
+
+let index = 0
+while (index <= 20) {
+    // console.log(`value of index: ${index}`);
+    index = index+2
+}
+
+let myArr = ["virat","Rohith","Dhoni","ABD"]
+
+let arr = 0
+while (arr < myArr.length) {
+    // console.log(`Value of array: ${myArr[arr]}`);
+    arr++
+}
+
+
+/****************************** do while ************************** */ 
+
+let score = 12
+
+do {
+    console.log(`Score is: ${score}`);
+    score++
+} while (score <= 10);
