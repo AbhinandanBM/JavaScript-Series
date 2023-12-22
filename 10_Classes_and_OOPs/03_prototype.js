a8cca0404f4d5bb3028b00e8d1d62f2a630b5007
AbhinandanBM <bmabhi13@gmail.com>
2023-12-22 21:28:03 +0530

    Prototype in JS

--- 10_Classes_and_OOPs/03_prototype.js
+++ 10_Classes_and_OOPs/03_prototype.js
@@ -2,24 +2,24 @@
 
 // console.log(myName.trueLength);
 
-let myhero = ["thor","spiderman"]
+let myhero = ["thor", "spiderman"];
 
 let heropower = {
-    thor: "hammer",
-    spiderman: "sling",
+  thor: "hammer",
+  spiderman: "sling",
 
-    getspiderPower: function(){
-        console.log(`spidy power ${this.spiderman}`);
-    }
-}
+  getspiderPower: function () {
+    console.log(`spidy power ${this.spiderman}`);
+  },
+};
 
-Object.prototype.abhi = function(){
-    console.log("abhi is present in all objects");
-}
+Object.prototype.abhi = function () {
+  console.log("abhi is present in all objects");
+};
 
-Array.prototype.heyAbhi = function(){
-    console.log("abhi says hello");
-}
+Array.prototype.heyAbhi = function () {
+  console.log("abhi says hello");
+};
 
 // heropower.abhi()
 // myhero.abhi()
@@ -29,36 +29,36 @@ Array.prototype.heyAbhi = function(){
 // inheritance
 
 const User = {
-    name: "chai",
-    email: "chai@google.com"
-}
+  name: "chai",
+  email: "chai@google.com",
+};
 
 const Teacher = {
-    makeVideo: true
-}
+  makeVideo: true,
+};
 
 const TeachingSupport = {
-    isAvailable: false
-}
+  isAvailable: false,
+};
 
 const TASupport = {
-    makeAssignment: 'JS assignment',
-    fullTime: true,
-    __proto__: TeachingSupport
-}
+  makeAssignment: "JS assignment",
+  fullTime: true,
+  __proto__: TeachingSupport,
+};
 
-Teacher.__proto__ = User
+Teacher.__proto__ = User;
 
 // modern syntax
-Object.setPrototypeOf(TeachingSupport,Teacher)
+Object.setPrototypeOf(TeachingSupport, Teacher);
 
-let anotherUser = "Abhinandan   "
+let anotherUser = "Abhinandan   ";
 
-String.prototype.trueLength = function(){
-    console.log(this);
+String.prototype.trueLength = function () {
+  console.log(this);
 
-    console.log(`True length string: ${this.trim().length}`);
-}
+  console.log(`True length string: ${this.trim().length}`);
+};
 
-anotherUser.trueLength()
-"chai".trueLength()
+anotherUser.trueLength();
+"chai".trueLength();
