8c58328005f769ba0f8c08febaf4bd11f5c5ac23
AbhinandanBM <bmabhi13@gmail.com>
2023-12-22 21:27:00 +0530

    Objects, Prototype

---
+++ 10_Classes_and_OOPs/03_prototype.js
@@ -0,0 +1,64 @@
+// let myName = "Abhinandan   "
+
+// console.log(myName.trueLength);
+
+let myhero = ["thor","spiderman"]
+
+let heropower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getspiderPower: function(){
+        console.log(`spidy power ${this.spiderman}`);
+    }
+}
+
+Object.prototype.abhi = function(){
+    console.log("abhi is present in all objects");
+}
+
+Array.prototype.heyAbhi = function(){
+    console.log("abhi says hello");
+}
+
+// heropower.abhi()
+// myhero.abhi()
+// myhero.heyAbhi()
+// heropower.heyAbhi()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport,Teacher)
+
+let anotherUser = "Abhinandan   "
+
+String.prototype.trueLength = function(){
+    console.log(this);
+
+    console.log(`True length string: ${this.trim().length}`);
+}
+
+anotherUser.trueLength()
+"chai".trueLength()
